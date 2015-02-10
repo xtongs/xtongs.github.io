ee4aff1e1984a69d15c86f661e09733a81bd5455
tony.shen <tony.shen@vipshop.com>
2015-02-10 14:04:20 +0800

    fix

--- src/script/index.js
+++ src/script/index.js
@@ -1,4 +1,13 @@
 $(function() {
+	setTimeout(function(){
+		var w = $('.section .fa').width();
+		if(w < parseInt($('body').css('font-size'))){
+			$('.section .fa').each(function(i,d){
+				$(d).addClass('nofont').css('margin-left',-w);
+				$(d).text($(d).parent().attr('title')[0]);
+			});
+		}
+	},2000);
 	touch.on($('.logo'), 'touchstart', function(e) {
 		e.stopPropagation();
 		e.preventDefault();
